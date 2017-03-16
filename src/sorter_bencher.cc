ecae8e216cf16ebe27d34a912bf930887a5a2a19
zhanglirich <zhanglirich@didichuxing.com>
2017-03-16 11:03:28 +0800

    change TValue to pair<double128,double128>

--- src/sorter_bencher.cc
+++ src/sorter_bencher.cc
@@ -2,8 +2,8 @@
 #include <cstdio>
 #include <ctime>
 #include <algorithm>
-#include <random>
 #include <vector>
+#include <random>
 #include <stdexcept>
 
 #include "algo_library_sort.hh"
@@ -17,12 +17,18 @@
 #include "sorter.hh"
 
 struct SorterBencher {
-    std::vector<long double> randomTestDataset;
-    std::vector<long double> randomTestDatasetSorted;
+    using TValue = std::pair<long double, long double>;
+    using TIndex = int;
+
+    std::vector<TValue> randomTestDataset;
+    std::vector<TValue> randomTestDatasetSorted;
 
     SorterBencher(int benchSize) {
-        std::generate_n(std::back_inserter(randomTestDataset), benchSize,
-                std::bind(std::uniform_real_distribution<long double>(0.0, 1.0), std::default_random_engine()));
+        auto randomDist = std::bind(std::uniform_real_distribution<long double>(), std::default_random_engine());
+        auto randomValueGenerator = [&]() {
+            return std::make_pair(randomDist(), randomDist());
+        };
+        std::generate_n(std::back_inserter(randomTestDataset), benchSize, randomValueGenerator);
         randomTestDatasetSorted = randomTestDataset;
         std::sort(randomTestDatasetSorted.begin(), randomTestDatasetSorted.end());
     }
@@ -31,16 +37,13 @@ struct SorterBencher {
         auto dataset = randomTestDataset;
         auto t1 = std::clock();
         {
-            sorter.template sort<std::vector<long double>::iterator, long double, int>(
-                    dataset.begin(),
-                    dataset.end());
+            sorter.template sort<std::vector<TValue>::iterator, TValue, TIndex>(dataset.begin(), dataset.end());
         }
         auto t2 = std::clock();
         auto timeCostMillis = (t2 - t1) * 1000 / CLOCKS_PER_SEC;
 
         if (dataset != randomTestDatasetSorted) {
             fprintf(stderr, "%s: bad sorting result\n", sorter.getAlgorithmName());
-            throw std::runtime_error("bad sorting result");
         }
         fprintf(stderr, "%-16s >>>> best: %-8s | avg: %-8s | worst: %-8s | space: %-8s >>>> cost time: %ldms\n",
                 sorter.getAlgorithmName(),
@@ -56,11 +59,11 @@ int main() {
     SorterBencher bencher(1000000);
     bencher.bench(Sorter<QuickSortAlgorithm>());
     bencher.bench(Sorter<MergeSortAlgorithm>());
+    bencher.bench(Sorter<InplaceMergeSortAlgorithm>());
     bencher.bench(Sorter<HeapSortAlgorithm>());
     bencher.bench(Sorter<ShellSortAlgorithm>());
     bencher.bench(Sorter<CombSortAlgorithm>());
     bencher.bench(Sorter<SmoothSortAlgorithm>());
-    bencher.bench(Sorter<InplaceMergeSortAlgorithm>());
     bencher.bench(Sorter<LibrarySortAlgorithm>());
     return 0;
 }
